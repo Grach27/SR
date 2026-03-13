3d6983b4aacae4a3ed06aafab83eccd95f663ce8
Grach67 <162838172+Grach27@users.noreply.github.com>
2026-03-13 22:45:06 +0300

    Implement sum calculation in SR12.cpp

---
+++ SR12.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num, sum = 0;
+
+    while (true) {
+
+        cin >> num;
+
+        if (num == 0)
+            break;
+
+        sum += num;
+    }
+
+    cout << "Сумма: " << sum;
+
+}
